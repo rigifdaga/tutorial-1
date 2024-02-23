30a75bc6ca0946bfdd4ad928f592c198b460f0ad
rigifdaga <rfdauliaa@gmail.com>
2024-02-23 16:52:37 +0700

    implement LSP

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/car")
-class CarController extends ProductController {
+public class CarController {
 
     @Autowired
     private CarService carService;
